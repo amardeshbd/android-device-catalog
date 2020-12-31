876a48ddc1c83a1333ecf7c667c340ed140a1620
Hossain Khan <amardeshbd@gmail.com>
2020-12-30 19:14:39 -0500

    [ADDED] Instrumentation test to test the database.

--- app/src/androidTest/java/dev/hossain/android/catalog/data/AppDatabaseInstTest.kt
+++ app/src/androidTest/java/dev/hossain/android/catalog/data/AppDatabaseInstTest.kt
@@ -3,16 +3,18 @@ package dev.hossain.android.catalog.data
 import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import dev.hossain.android.catalog.data.model.Device
+import java.io.IOException
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
-class SimpleEntityReadWriteTest {
+class AppDatabaseInstTest {
     private lateinit var dao: DeviceDao
     private lateinit var db: AppDatabase
 
@@ -37,12 +39,16 @@ class SimpleEntityReadWriteTest {
         val user: Device = TestUtil.createDevice()
 
         dao.insert(user)
-        val byName = dao.getAllDeviceInfo()
+        val allDevice = dao.getAllDeviceInfo()
+
+        assertThat(allDevice.size, equalTo(1))
+
+        assertThat(allDevice[0].device.modelName, equalTo("ModelX"))
     }
 
     object TestUtil {
         fun createDevice(): Device {
-            return Device()
+            return Device(modelName = "ModelX")
         }
     }
 }
